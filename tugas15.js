f75b858e6806f9003c47b8af17eda421ad8230a6
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-09-21 10:14:57 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+var AB = 8
+var BC = 6
+var hasil;
+console.log("Nilai AB : 8")
+console.log("Nilai BC : 6")
+
+rumussegitiga = (Math.pow(8,2))+(Math.pow(6,2))
+hasil = Math.sqrt(rumussegitiga)
+
+console.log("Panjang sisi AC pada segitiga tesebut adalah :", hasil)
+	
